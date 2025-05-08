441d240070664e812cffe85538cc2c74b6b2ee96
Alex Johnstone <alexjj@gmail.com>
2025-05-08 08:48:36 +0100

    accept user input for park prefix

--- newparks.py
+++ newparks.py
@@ -4,9 +4,17 @@
 # See the README.md file for more details.
 # This is Public Domain software, see the LICENCE file
 
+import sys
 from requests_cache import CachedSession
 from datetime import timedelta
 import simplekml
+
+# Ensure user provided a prefix
+if len(sys.argv) != 2:
+    print("Usage: python newparks.py <programPrefix>")
+    sys.exit(1)
+
+input_prefix = sys.argv[1].upper()
 
 # Create KML object to fill with unactivated parks
 kml = simplekml.Kml()
@@ -14,22 +22,28 @@ kml = simplekml.Kml()
 # Fetch list of programs
 session = CachedSession("newparks_cache", expire_after=timedelta(days=1))
 programs = session.get("https://api.pota.app/programs").json()
-print("Found " + str(len(programs)) + " POTA programs")
 
-# For each program, fetch a list of parks
-for program in programs:
-    prefix = program["programPrefix"]
-    parks = session.get("https://api.pota.app/program/parks/" + prefix).json()
-    print("Found " + str(len(parks)) + " parks in " + prefix)
+# Validate the input prefix
+valid_prefixes = {program["programPrefix"]: program for program in programs}
+if input_prefix not in valid_prefixes:
+    print(f"Invalid programPrefix '{input_prefix}'. Valid options are: {', '.join(valid_prefixes.keys())}")
+    sys.exit(1)
 
-    # For each park, if it's not activated, store its details in the KML
-    for park in parks:
-        if park["activations"] == 0:
-            kml.newpoint(name=park["reference"] + " " + park["name"],
-                         description="https://pota.app/#/park/" + park["reference"],
-                         coords=[(park["longitude"], park["latitude"])])
+print(f"Fetching parks for program prefix: {input_prefix}")
+
+# Fetch parks for the specified prefix
+parks = session.get(f"https://api.pota.app/program/parks/{input_prefix}").json()
+print(f"Found {len(parks)} parks in {input_prefix}")
+
+# Add unactivated parks to KML
+for park in parks:
+    if park["activations"] == 0:
+        kml.newpoint(name=f'{park["reference"]} {park["name"]}',
+                     description=f'https://pota.app/#/park/{park["reference"]}',
+                     coords=[(park["longitude"], park["latitude"])])
 
 # Save KML
-print("Writing to newparks.kml...")
-kml.save("newparks.kml")
+filename = f"newparks-{input_prefix}.kml"
+print(f"Writing to {filename}...")
+kml.save(filename)
 print("Done.")
